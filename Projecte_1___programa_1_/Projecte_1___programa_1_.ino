4f7ea1f9a598429cc2d01f3dcde2c2637230c954
Cro1995 <rubiocarlos327@gmail.com>
2016-02-15 16:26:17 +0100

    Projecte 1 ( programa 1)

---
+++ Projecte_1___programa_1_/Projecte_1___programa_1_.ino
@@ -0,0 +1,26 @@
+/*************************************************************************
+**                                                                      **
+**              Descripció programa                                     **
+**                                                                      **
+*************************************************************************/
+
+//******  Includes   *****************************************************
+
+
+//******  Variables  *****************************************************
+
+
+//******  Setup  *********************************************************
+
+void setup()                // run once, when the sketch starts
+{
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.println("Hello world!"); // prints with brake line
+}
+//En aquest programa ens podem comunicar amb el arduino, a partir del serial println que despres podem posar el que nosaltres volguem 
+
+
+//******  loop  **********************************************************
+void loop() {
+  
+}
