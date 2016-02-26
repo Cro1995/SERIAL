28462cadb3471217c4d948a55e9920124eef4b3d
Cro1995 <rubiocarlos327@gmail.com>
2016-02-26 19:25:45 +0100

    serial

---
+++ prog2.1/prog2.1.ino
@@ -0,0 +1,31 @@
+
+int a = 32;
+int b = 45;
+int c = 59;
+int d = 61;
+
+
+void setup() {     // put your setup code here, to run once:
+
+ Serial.begin(9600); 
+
+ Serial.println("Unes quantes operacions: ");
+
+ Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+  Serial.print("c = ");
+  Serial.println(c);
+  Serial.print("d = ");
+  Serial.println(d);
+
+  Serial.print("a + c = ");
+  Serial.println(a + c); 
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
